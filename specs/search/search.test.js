f206e7e8ee95986ef278c984138a7d5633dfd5ce
Scott Cook <scott.madison.cook@gmail.com>
2023-10-31 12:46:16 -0700

    Add linary and binary search solutions

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -5,10 +5,36 @@
 
 function linearSearch(id, array) {
   // code goes here
+  let result;
+  array.filter((item) => {
+    if (item.id == id) {
+      result = item;
+    } 
+  })
+  return result;
 }
 
 function binarySearch(id, array) {
-  // code goes here
+  // Set a min and max (length of array)
+  let min = 0;
+  let max = array.length - 1;
+  // Set an index and element.
+  let index;
+  let element;
+  // while index is less than max
+  while (min <= max) {
+    index = Math.floor((min + max) / 2);
+    element = array[index]
+    
+    if (element.id < id) {
+      min = index + 1;
+    } else if (element.id > id) {
+      max = index - 1;
+    } else {
+      return element;
+    }
+  }
+  return undefined;
 }
 
 // unit tests
@@ -35,7 +61,7 @@ test.skip("linear search", function () {
   ).toBe(lookingFor);
 });
 
-test.skip("binary search", function () {
+test("binary search", function () {
   const lookingFor = { id: 23, name: "Brian" };
   expect(
     binarySearch(23, [
